c22be2920230f1d6463611ce419456982f665d66
Radek Šťasta <radek.stasta@gmail.com>
2024-07-13 14:59:13 +0200

    Added header and content parts to page, added sticky header functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleViewerComponent } from '../components/article-viewer/article-viewer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ArticleViewerComponent],
+  imports: [RouterOutlet, ArticleViewerComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
 export class AppComponent {
-  title = 'website-personal';
+  @ViewChild('stickyHeader', { static: false }) headerElement!: ElementRef;
+  public isHeaderSticky = false;
+
+  get headerClasses() {
+    return {
+      'duration-500 translate-y-full': this.isHeaderSticky, // Apply when header is sticky
+      'duration-0 default-top': !this.isHeaderSticky, // Apply when header is sticky
+    };
+  }
+
+  @HostListener('window:scroll', [])
+  checkScroll() {
+    if (window.scrollY >= this.headerElement.nativeElement.scrollHeight) {
+      this.isHeaderSticky = true;
+      this.headerElement.nativeElement.classList.add('sticky');
+    } else {
+      this.isHeaderSticky = false;
+    }
+
+    if (window.scrollY == 0) {
+      this.headerElement.nativeElement.classList.remove('sticky');
+    }
+  }
 }
