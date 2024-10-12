94f8401779fccbea20ebab386e558237140e8aa5
Radek Šťasta <radek.stasta@gmail.com>
2024-10-12 23:13:21 +0200

    Added articles component, added simple routing to menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ArticlesComponent } from './components/articles/articles.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'articles',
+    component: ArticlesComponent,
   },
 ];
---
+++ src/app/components/articles/articles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articles',
+  standalone: true,
+  imports: [],
+  templateUrl: './articles.component.html',
+  styleUrl: './articles.component.sass',
+})
+export class ArticlesComponent {}
