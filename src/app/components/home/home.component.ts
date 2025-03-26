58e5eb2c2ba9f5a80780ae63c4b8332b82a9fbd3
Radek Šťasta <radek.stasta@gmail.com>
2025-03-26 15:48:30 +0100

    Added home component, updated .launch.json

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.sass',
+})
+export class HomeComponent {}
