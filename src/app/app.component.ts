085572dc0d72944f68193c6d3da60901a7998c38
Radek Šťasta <radek.stasta@gmail.com>
2025-04-02 22:37:54 +0200

    Added theme customizing
    Improves visual appeal by applying the Nord theme.
    
    Updates the home page to use the primary color from the theme for the header and card background, enhancing visual consistency.
    
    Renames component stylesheet for consistency.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'radek-stasta.github.io';
