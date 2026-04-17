982d01e448fa4f6ff5c6e5bb74bc305dc98f8428
Radek Šťasta <radek.stasta@gmail.com>
2026-04-17 09:44:56 +0200

    Update home page with personal bio and site description
    Replace the placeholder description with an actual introduction covering professional background, education, and the purpose of the website as a project hub.

--- personal-website-react-nextjs/app/page.tsx
+++ personal-website-react-nextjs/app/page.tsx
@@ -27,7 +27,14 @@ export default function Home() {
           {/* Bottom row - description */}
           <div className="flex-1 flex items-end">
             <p className="text-sm text-fg/70">
-              Brief description about me and what I do goes here.
+              Hi. My name is Radek and I'm {currentDate.getFullYear() - birthday.getFullYear()} years old. Practically my whole life, I've been passionate about
+              computers and whole IT world. That passion resulted in magister degree in Computer Science and then in all my professional career.
+              Throughout the years, I have touched many different areas of IT, both as my professional and also personal interest.
+
+              This website will serve as a hub for all my projects, ideas, articles and other content that I want to share with the world and also as
+              my portfolio of my work. 
+              
+              Currently there is not much content here, but I will be adding more and more over time, so stay tuned!
             </p>
           </div>
         </div>
